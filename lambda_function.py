15bc22fb9da11898d16b96b64d2796e4c1d2d3ed
Haydn Nitzsche <42683697+HaydnNitzsche@users.noreply.github.com>
2019-01-20 00:42:09 -0700

    Works better

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,7 @@ import asyncio
 tournament = None
 participants = []
 matches = None
+names = []
 
 #Startup
 #--------------------------------------------------
@@ -12,6 +13,7 @@ async def login(tournament_name = "HackAZ"):
     global tournament
     global participants
     global matches
+    global names
 
     USER = "haydunce" 
     KEY = "yCYu1uKVX10iNrRh5vJfy48ReZC2iQ0Kchi4xzMs"
@@ -20,17 +22,24 @@ async def login(tournament_name = "HackAZ"):
     tournament = await user.get_tournament(url = tournament_name)
     participants = await tournament.get_participants()
     matches = await tournament.get_matches()
+    
+    for p in participants:
+        names.append(p.name)
 
-async def lambda_handler(event, context):
+def lambda_handler(event, context):
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(handler(loop, event, context))
+    
+async def handler(loop, event, context):
     await login()
     
     if event['request']['type'] == "LaunchRequest":
-        return on_launch(event, context)
+        return await on_launch(event, context)
 
     elif event['request']['type'] == "IntentRequest":
-        return intent_router(event, context)
+        return await intent_router(event, context)
 
-def on_launch(event, context):
+async def on_launch(event, context):
     return statement("easy bracket","I'll do my best to help you with your tournament")
 
 async def intent_router(event, context):
@@ -61,8 +70,17 @@ async def intent_router(event, context):
         return await reset_tournament()
     
     if intent == "EndMatch":
+        global names
+        winner = event['request']['intent']['slots']['player']['value']
         
-        return await update_match(get_next_match().id, )
+        if winner is in names:
+            participant = await tournament.get_participant(await get_id_from_participant(winner))
+            match = participant.get_next_match()
+            await update_match(match.id, participant.id)
+            return statement("this is dumb", "Okay, so " + winner + " is a winner")
+        else:
+            return statement("Not in Tourney", "That person is not in the tournament")
+        
 
 #Custom Intents Functions
 #--------------------------------------------------
@@ -93,13 +111,15 @@ async def get_next_match():
 async def next_open_match():
     current_match = await get_next_match()
 
-    player1 = await tournament.get_participant(current_match.player1_id)
-    player2 = await tournament.get_participant(current_match.player2_id)
+    part1 = await tournament.get_participant(current_match.player1_id)
+    part2 = await tournament.get_participant(current_match.player2_id)
+    player1 = part1.name
+    player2 = part2.name
 
     return statement("Next Match",("The next match is between " + player1 + " and " + player2))
 
 #attempts to set the completed score/outcome
-async def update_match(match_id, winner_id, scores = ""):
+async def update_match(match_id, winner_id, scores = "0-0"):
     global tournament
     winner = await tournament.get_participant(winner_id)
     match = await tournament.get_match(match_id)
@@ -193,7 +213,6 @@ def rules():
 async def get_id_from_participant(name):
     global participants
     for p in (participants):
-        print(p.name)
         if p.name == name:
             return p.id
     return None
